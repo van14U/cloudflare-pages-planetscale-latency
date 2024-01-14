c5111d2b01b021f2c743b5378ecbb9da2f927225
Ivan B <sensenabi@gmail.com>
2024-01-14 18:13:15 -0500

    add test

---
+++ src/app/_components/actions.ts
@@ -0,0 +1,52 @@
+"use server"
+
+import { Client } from "@planetscale/database";
+
+const us_east_1_connection = new Client({
+  url: process.env.DATABASE_URL_US_EAST_1 as string,
+  fetch: (url, init) => {
+    if (init) {
+      delete init.cache;
+    }
+    return fetch(url, init);
+  },
+}).connection();
+
+const sa_east_1_connection = new Client({
+  url: process.env.DATABASE_URL_SA_EAST_1 as string,
+  fetch: (url, init) => {
+    if (init) {
+      delete init.cache;
+    }
+    return fetch(url, init);
+  },
+}).connection();
+
+const QUERY = "SELECT * FROM `t3-app_post` ORDER BY id DESC LIMIT 1";
+
+export type BenchResult = {
+  latencies: number[];
+} | undefined;
+
+export async function bench(prev: BenchResult, data: FormData): Promise<BenchResult> {
+  const region = data.get("region") as string | undefined;
+  if (!region || (region !== "us-east-1" && region !== "sa-east-1")) {
+    return
+  }
+  const connection = region === "us-east-1" ? us_east_1_connection : sa_east_1_connection;
+  const start = Date.now();
+  await connection.execute(QUERY);
+  const firstQueryLatency = Date.now() - start;
+
+  const latencies = new Array<number>(10);
+  latencies[0] = firstQueryLatency;
+  for (let i = 0; i < 9; i++) {
+    const start = Date.now();
+    await connection.execute(QUERY);
+    latencies[i + 1] = Date.now() - start;
+    // await new Promise((resolve) => setTimeout(resolve, 100));
+  }
+  console.log(latencies);
+
+  return { latencies };
+}
---
+++ src/app/_components/benchmark.tsx
@@ -0,0 +1,65 @@
+"use client"
+
+import { useFormState, useFormStatus } from "react-dom"
+import { bench } from "./actions"
+import { type ButtonHTMLAttributes } from "react"
+
+function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+  const { pending } = useFormStatus()
+  return (
+    <button
+      {...props}
+      disabled={pending}
+      className={`border disabled:opacity-70 rounded-md px-3 py-1 bg-neutral-900 dark:bg-white dark:text-black ${props.className || ""
+        }`}
+    />
+  )
+}
+
+function Results(props: { results?: { latencies: number[] } }) {
+
+  if (!props.results?.latencies) {
+    return null
+  }
+
+  const rawResult = structuredClone(props.results.latencies);
+  const copy = structuredClone(props.results.latencies);
+  const median = copy.sort()[Math.floor(copy.length / 2)]
+  const avg = copy.reduce((a, b) => a + b, 0) / copy.length
+  const max = copy.reduce((a, b) => Math.max(a, b), 0)
+  const min = copy.reduce((a, b) => Math.min(a, b), Infinity)
+
+  return (
+    <div className="grid gap-2">
+      {rawResult.map(lat => (
+        <div key={Math.random()}>{lat}ms</div>
+      ))}
+      <div>Median: {median}ms</div>
+      <div>Average: {avg}ms</div>
+      <div>Max: {max}ms</div>
+      <div>Min: {min}ms</div>
+    </div>
+  )
+}
+
+export function Bench() {
+  const [state, formAction] = useFormState(bench, undefined);
+
+  return (
+    <div>
+      <form action={formAction} className="grid gap-2" >
+        <select name="region" className="dark:bg-black rounded-md border px-3 py-1">
+          <option value="us-east-1">us-east-1</option>
+          <option value="sa-east-1">sa-east-1</option>
+        </select>
+        <Button
+          type="submit"
+          className="border rounded-md px-3 py-1 bg-neutral-900 dark:bg-white dark:text-black"
+        >
+          Measure
+        </Button>
+      </form>
+      <Results key={JSON.stringify(state?.latencies)} results={state} />
+    </div>
+  )
+}
